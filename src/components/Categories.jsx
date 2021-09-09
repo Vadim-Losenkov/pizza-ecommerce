abfb966c23ba4c6dcdc5b30ca820a6ab7f4de770
Vadim Losenkov <vProg0729@gmail.com>
2021-09-09 14:46:23 +0300

    end lesson 3

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -35,15 +35,19 @@ import React from 'react'
 function Categories({items, onClick}) {
   const [activeItem, setActiveState] = React.useState(null)
 
+  const onSelectItem = (index) => {
+    setActiveState(index)
+  }
+
   return (
     <div className="categories">
       <ul>
-        <li className="active">Все</li>
-        {items.map((name, index) => (
+        <li className={activeItem === null ? 'active' : ''} onClick={() => onSelectItem(null)}>Все</li>
+        {items && items.map((name, index) => (
           <li 
             className={activeItem === index ? 'active' : ''}
             key={`${name}_${index}`}
-            onClick={() => setActiveState(index)}
+            onClick={() => onSelectItem(index)}
           >{name}</li>
         ))}
       </ul>
